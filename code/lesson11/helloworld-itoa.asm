7e825affd5d1d9105b0f460d24a9a540532d2c75
Daniel Givney <daniel@givneysoftware.com.au>
2013-03-30 13:40:37 +1100

    Add code for lesson 11. Hard code call instruction spans in lesson 4.

---
+++ code/lesson11/helloworld-itoa.asm
@@ -0,0 +1,22 @@
+; Hello World Program (Integer to ASCII ITOA)
+; Compile with: nasm -f elf helloworld-itoa.asm
+; Link with (64 bit systems require elf_i386 option): ld -m elf_i386 helloworld-itoa.o -o helloworld-itoa
+; Run with: ./helloworld-itoa
+
+%include        'functions.asm'
+
+SECTION .text
+global  _start
+    
+_start: 
+
+    mov     ecx, 0
+    
+nextNumber:
+    inc     ecx
+    mov     eax, ecx
+    call    iprintLF        ; NOTE call our new integer printing function (itoa)
+    cmp     ecx, 10
+    jne     nextNumber
+      
+    call    quit
