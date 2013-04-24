7a94b8ec6803e15bd08496f289b143909724bdca
DGivney <daniel@givneysoftware.com.au>
2013-04-25 09:20:58 +1000

    Fix formatting issue.

--- code/lesson8/helloworld-args.asm
+++ code/lesson8/helloworld-args.asm
@@ -7,18 +7,18 @@
 
 SECTION .text
 global  _start
-    
-_start: 
+
+_start:
 
     pop     ecx             ; first value on the stack is the number of arguments
-    
+
 nextArg:
     cmp     ecx, 0H         ; check to see if we have any arguments left
     jz      noMoreArgs      ; if zero flag is set jump to noMoreArgs label (by jumping over the end of the loop)
     pop     eax             ; pop the next argument off the stack
     call    sprintLF        ; call our print with linefeed function
-    dec		ecx             ; decrease edi (number of arguments left) by 1
+    dec     ecx             ; decrease edi (number of arguments left) by 1
     jmp     nextArg         ; jump to nextArg label
-    
-noMoreArgs:   
+
+noMoreArgs:
     call    quit
