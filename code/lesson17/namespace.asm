64e22b73731d067c9101a95866d7206da07c5a7f
DGivney <daniel@givneysoftware.com.au>
2013-05-25 12:21:44 +1000

    Add content for lesson17 - namespace.

--- code/lesson17/namespace.asm
+++ code/lesson17/namespace.asm
@@ -6,8 +6,9 @@
 %include        'functions.asm'
 
 SECTION .data
-msg1        db      'Jumping to finished label.', 0h     ; a message string
-msg2        db      'Inside subroutine number: ', 0h     ; a message string
+msg1        db      'Jumping to finished label.', 0h        ; a message string
+msg2        db      'Inside subroutine number: ', 0h        ; a message string
+msg3        db      'Inside subroutine "finished".', 0h     ; a message string
 
 SECTION .text
 global  _start
@@ -15,30 +16,32 @@ global  _start
 _start:
 
 subrountineOne:
-    mov     eax, msg1
-    call    sprintLF
-    jmp     .finished
+    mov     eax, msg1       ; move the address of msg1 into eax
+    call    sprintLF        ; call our string printing with linefeed function
+    jmp     .finished       ; jump to the local label under the subrountineOne scope
 
 .finished:
-    mov     eax, msg2
-    call    sprint
-    mov     eax, 1
-    call    iprintLF
+    mov     eax, msg2       ; move the address of msg2 into eax
+    call    sprint          ; call our string printing function
+    mov     eax, 1          ; move the value one into eax (for subroutine number one)
+    call    iprintLF        ; call our integer printing function with linefeed function
 
 subrountineTwo:
-    mov     eax, msg1
-    call    sprintLF
-    jmp     .finished
+    mov     eax, msg1       ; move the address of msg1 into eax
+    call    sprintLF        ; call our string print with linefeed function
+    jmp     .finished       ; jump to the local label under the subrountineTwo scope
 
 .finished:
-    mov     eax, msg2
-    call    sprint
-    mov     eax, 2
-    call    iprintLF
+    mov     eax, msg2       ; move the address of msg2 into eax
+    call    sprint          ; call our string printing function
+    mov     eax, 2          ; move the value two into eax (for subroutine number two)
+    call    iprintLF        ; call our integer printing function with linefeed function
 
-    mov     eax, msg1
-    call    sprintLF
-    jmp     finished
+    mov     eax, msg1       ; move the address of msg1 into eax
+    call    sprintLF        ; call our string printing with linefeed function
+    jmp     finished        ; jump to the global label finished
 
 finished:
-    call    quit
+    mov     eax, msg3       ; move the address of msg3 into eax
+    call    sprintLF        ; call our string printing with linefeed function
+    call    quit            ; call our quit function
