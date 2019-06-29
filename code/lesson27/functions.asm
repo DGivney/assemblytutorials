ba94d98af425cf13e70993c1ade611ac2fd19888
DGivney <daniel@givneysoftware.com.au>
2019-06-29 17:45:26 +1000

    Remove superfluous checks in atoi function #17

--- code/lesson27/functions.asm
+++ code/lesson27/functions.asm
@@ -17,10 +17,6 @@ atoi:
     jl      .finished       ; jump if less than to label finished
     cmp     bl, 57          ; compare ebx register's lower half value against ascii value 57 (char value 9)
     jg      .finished       ; jump if greater than to label finished
-    cmp     bl, 10          ; compare ebx register's lower half value against ascii value 10 (linefeed character)
-    je      .finished       ; jump if equal to label finished
-    cmp     bl, 0           ; compare ebx register's lower half value against decimal value 0 (end of string)
-    jz      .finished       ; jump if zero to label finished
 
     sub     bl, 48          ; convert ebx register's lower half to decimal representation of ascii value
     add     eax, ebx        ; add ebx to our interger value in eax
